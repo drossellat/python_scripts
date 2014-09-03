b96a60dacf487fdc0b0c56fa94194daa9523c7b0
David Rossellat <david.rossellat@gmail.com>
2014-09-03 14:40:17 +0700

    tweaking authorizedusers

--- firebase-py/firebase-data-extractor.py
+++ firebase-py/firebase-data-extractor.py
@@ -47,7 +47,7 @@ def transform(data):
   elif data.has_key('authorizedUsers'):
     unix_timestamp = str(data['createdAt'])[0:-3]
     data['createdAt'] = unix_timestamp
-    data['authorizedUsers']= data['authorizedUsers']
+    #data['authorizedUsers']= data['authorizedUsers']
   return data
 
 #main 
